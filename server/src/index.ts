a0ac64755d014bb68935273f92aefb36b9b52111
@arxlan99 <ozkan.arslan.321@gmail.com>
2022-09-14 22:43:22 +0300

    Add cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,7 +17,7 @@ const port = process.env.PORT || 4000;
 app.use(
   cors({
     credentials: true,
-    origin: ['http://localhost:3000'],
+    origin: ['https://retwitter.vercel.app'],
   })
 );
 
